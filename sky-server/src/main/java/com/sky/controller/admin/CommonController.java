b99140db0b144ea41b803bd8917ab05a9ef556d7
Andyo0O0o <1260098198@qq.com>
2026-02-06 14:08:59 +0800

    Merge remote-tracking branch 'origin/master'

--- sky-server/src/main/java/com/sky/controller/admin/CommonController.java
+++ sky-server/src/main/java/com/sky/controller/admin/CommonController.java
@@ -1,13 +1,18 @@
 package com.sky.controller.admin;
 
 import com.sky.result.Result;
+import com.sky.utils.AliOssUtil;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.UUID;
 
 /**
  * 通用接口
@@ -17,6 +22,10 @@ import org.springframework.web.multipart.MultipartFile;
 @Slf4j
 @Api(tags = "通用接口")
 public class CommonController {
+
+    // 通过@Autowired直接注入Spring容器中的Bean
+    @Autowired
+    private AliOssUtil aliOssUtil;
 
     /**
      * 文件上传
@@ -28,9 +37,20 @@ public class CommonController {
     public Result<String> upload(MultipartFile file){
         log.info("文件上传:{}",file);
 
-
-
-        return null;
+        try {
+            //原始文件名
+            String originalFilename = file.getOriginalFilename();
+            //截取原始文件的后缀
+            String suffix = originalFilename.substring(originalFilename.lastIndexOf("."));
+            //生成新文件名
+            String objectName = UUID.randomUUID().toString() + suffix;
+            //文件的请求路径
+            String filePath = aliOssUtil.upload(file.getBytes(), objectName);
+            return Result.success(filePath);
+        } catch (IOException e) {
+            log.error("文件上传失败:{}",e.getMessage());
+            return Result.error("文件上传失败");
+        }
     }
 
 }
